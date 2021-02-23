3f771dc1418c01ca568a2f74d4c4811736c71091
mkornaukhov03 <mkornaukhov03@yandex.ru>
2021-02-24 01:21:57 +0300

    Empty implementation in interface.h

--- include/autocomplete/interface.h
+++ include/autocomplete/interface.h
@@ -2,44 +2,43 @@
 #define BATON_INTERFACE_H
 
 #include <QObject>
-#include <QTimer>
 #include <QProcess>
+#include <QTimer>
 #include <string>
 #include <vector>
 
-//  1) Need a RO access to file + position of cursor
+// class to parse from json to C++/Qt containers
 
-// comment because of undefined reference
-//class LSPHandler : public QObject {
-//  Q_OBJECT
-//
-// public:
-//  LSPHandler(LSPHandler &&) = delete;
-//  LSPHandler(LSPHandler &) = delete;
-//
-//  LSPHandler &operator=(LSPHandler &&) = delete;
-//  LSPHandler &operator=(LSPHandler &) = delete;
-//
-//  ~LSPHandler() final;
-//
-// signals:
-//  void completion(const std::vector<std::string> &);
-//  void diagnostic(const std::vector<std::string> &);
-//
-// public slots:
-//  void OnNotify(const QString &method, const QJsonObject &param);
-//  void OnResponse(const QJsonObject &id, const QJsonObject &response);
-//  void OnRequest(const QString &method, const QJsonObject &param,
-//                 const QJsonObject &id);
-//  void OnError(const QJsonObject &id, const QJsonObject &error);
-//  void OnServerError(QProcess::ProcessError error);
-//  void OnServerFinished(int exitCode, QProcess::ExitStatus status);
-//
-//  void textChanged();
-//  void requestDiagonistics();
-//
-// private:
-//  void set_connections();
-//};
+class LSPHandler : public QObject {
+  Q_OBJECT
+
+ public:
+  LSPHandler(LSPHandler &&) = delete;
+  LSPHandler(LSPHandler &) = delete;
+
+  LSPHandler &operator=(LSPHandler &&) = delete;
+  LSPHandler &operator=(LSPHandler &) = delete;
+
+  ~LSPHandler() final = default;
+
+ signals:
+  void completion(const std::vector<std::string> &);
+  void diagnostic(const std::vector<std::string> &);
+
+ public slots:
+  void OnNotify(const QString &, const QJsonObject &){};
+  void OnResponse(const QJsonObject &, const QJsonObject &){};
+  void OnRequest(const QString &, const QJsonObject &,
+                 const QJsonObject &){};
+  void OnError(const QJsonObject &, const QJsonObject &){};
+  void OnServerError(QProcess::ProcessError ){};
+  void OnServerFinished(int , QProcess::ExitStatus){};
+
+  void textChanged(){};
+  void requestDiagonistics(){};
+
+ private:
+  void set_connections(){};
+};
 
 #endif
