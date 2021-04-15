ef5adf7df0aa63b87cea2f6eaae0464f63e5d81b
Andrey <Stepanov.andre@gmail.com>
2021-04-15 16:16:40 +0300

    Corrected header files

--- include/autocomplete/interface.h
+++ include/autocomplete/interface.h
@@ -6,8 +6,10 @@
 #include <QTimer>
 #include <string>
 #include <vector>
-
+#include "client.h"
 // class to parse from json to C++/Qt containers
+
+namespace lsp {
 
 class LSPHandler : public QObject {
   Q_OBJECT
@@ -19,25 +21,33 @@ class LSPHandler : public QObject {
   LSPHandler &operator=(LSPHandler &&) = delete;
   LSPHandler &operator=(LSPHandler &) = delete;
 
-  ~LSPHandler() final = default;
+  LSPHandler(const std::string& root, const std::string& file_name, const std::string & content);
+
+  ~LSPHandler() final;
 
  signals:
-  void completion(const std::vector<std::string> &);
-  void diagnostic(const std::vector<std::string> &);
+  void DoneCompletion(const std::vector<std::string> &);
+  void DoneDiagnostic(const std::vector<std::string> &);
 
  public slots:
-  void OnNotify(const QString &, const QJsonObject &) {}
-  void OnResponse(const QJsonObject &, const QJsonObject &) {}
-  void OnRequest(const QString &, const QJsonObject &, const QJsonObject &) {}
-  void OnError(const QJsonObject &, const QJsonObject &) {}
-  void OnServerError(QProcess::ProcessError) {}
-  void OnServerFinished(int, QProcess::ExitStatus) {}
+  // from LSP client, connected inside
+  void GetNotify(const std::string &, json) { }
+  void GetResponse(json, json);
+  void GetRequest(const std::string &, json, json) { }
+  void GetError(json, json) { }
+  void GetServerError(QProcess::ProcessError) { }
+  void GetServerFinished(int, QProcess::ExitStatus) { }
+  void GetStderrOutput(const std::string &) { }
 
-  void textChanged() {}
-  void requestDiagonistics() {}
+  // from user
+  void RequestCompletion(std::size_t, std::size_t);
+  void FileChanged(const std::string & new_content, std::size_t, std::size_t);
 
  private:
-  void set_connections() {}
+  std::string root_;
+  std::string file_;
+  Client client_;
+  void set_connections();
 };
-
+}// namespace lsp
 #endif
